63d3a98df8e8d908f9a5af5fe61543720a48205a
aaron555 <mahanability@gmail.com>
2015-10-14 20:57:42 +0700

    Done

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+def fizzbuzz(divide_me)
+
+  if (divide_me % 3 == 0) && (divide_me % 5 == 0)
+    "FizzBuzz"
+  elsif divide_me % 3 == 0    
+    "Fizz"
+  elsif divide_me % 5 == 0    
+    "Buzz"
+  else 
+    
+  end
+
+end
