b2ddfd7d7864dc651748e14c40c1096af65c0f53
tyrantdavis <davistyrant@gmail.com>
2015-11-03 04:50:10 -0600

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(number)
+  if number % 15 == 0 # if the number is divisible by 3 and 5
+    "FizzBuzz" # print FizzBuzz
+  elsif number % 5 == 0 # if the number is divisible by 5
+    "Buzz" # return Buzz.
+  elsif number % 3 == 0 # if the number is divisible by 3 
+    "Fizz" # return Fizz
+  end
+end
+
