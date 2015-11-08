cca95cbdcbe142899bee0a45b3f02136a0349800
pacosta613 <pedro.acosta613@gmail.com>
2015-11-07 21:41:28 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(number)
   return "FizzBuzz" if is_divisible_by_fifteen?(number)
   return "Buzz" if is_divisible_by_five?(number)
   return "Fizz" if is_divisible_by_three?(number)
-  number
+
 end
 
 def is_divisible_by_three?(number)
