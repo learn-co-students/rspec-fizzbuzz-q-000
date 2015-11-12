55de224d723799b8139a1322e262eb7d55c98598
scottyhoffman2 <scottyhoffman2@gmail.com>
2015-11-12 14:46:59 -0500

    Done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 5 == 0
+      "FizzBuzz"
+    else
+      "Fizz"
+    end
+    
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    return nil
+  end
 end
