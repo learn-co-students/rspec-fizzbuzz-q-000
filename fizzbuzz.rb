fd541d20d318cda544f0911d5257cf233c345280
mailethiesen <mailethiesen@gmail.com>
2015-11-21 22:01:14 +0000

    nitrous-backup at Sat Nov 21 22:01:14 UTC 2015

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisble by 3 and 5
     "FizzBuzz" # Go FizzBuzz
   elsif int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    "Fizz" # Go Fizz
   elsif int % 5 == 0 # if the number int is divisble by 5
     "Buzz" # Go Buzz
   else puts "nil"
