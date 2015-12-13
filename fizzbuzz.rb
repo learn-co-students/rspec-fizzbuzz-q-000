1dee821f75fa1d98ac81d49b0132eb6993cfd872
jaredwerba <jwerba@live.com>
2015-12-12 20:40:32 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    "fizz"
-  end
-
-  if int % 5 == 0
-    "buzz"
-  end
-
-  if int % 15 == 0
+  if  int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+  elsif int % 3 == 0
+     "Fizz"
+  elsif int % 5 == 0
+      "Buzz"
   end
-
-end
+end 
