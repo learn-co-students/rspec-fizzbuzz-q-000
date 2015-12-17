6c3c4f694ed53b17546c568d031979ffd15cd5a1
cryanst <cryanst@gmail.com>
2015-12-17 03:10:09 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz(int)
+  if int%3==0&&int%5==0
+    "FizzBuzz"
+  elsif int%5==0
+    "Buzz"
+  elsif int%3==0
+    "Fizz"
+  else
+    
+  end
+end
+
+
+
+fizz_3=fizzbuzz(3)
+fizz_3
+fizz_5=fizzbuzz(5)
+fizz_5
+fizz_15=fizzbuzz(15)
+fizz_15
