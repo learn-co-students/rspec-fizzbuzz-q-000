754d59e1cb3fc08e124a79c7d8754cd6f0d44f45
nscarlyon <natasha.carlyon@gmail.com>
2015-12-23 22:44:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
-def fizzbuzz(number)
+def fizzbuzz(int)
 
-        If number % 3 == 0
+if int % 3 == 0 && int % 5 == 0
+        "FizzBuzz"
+
+       elsif int % 3 == 0
   "Fizz"
 
-elsif number % 5 == 0
+elsif int % 5 == 0
         "Buzz"
 
-elsif number % 3 == 0 && number % 5 == 0
-        "FizzBuzz"
 end
 end
 
