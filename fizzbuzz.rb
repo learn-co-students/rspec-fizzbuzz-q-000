fc4ec2d3baf09cb9b9c902dccc7c564198575cb1
tmharkins <tmharkins@gmail.com>
2015-12-24 01:15:13 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(int)
+  if (int.to_i % 3 == 0) && (int.to_i % 5 == 0)
+    return "FizzBuzz"
+  elsif (int.to_i % 3 == 0) && (int.to_i % 5 != 0)
+    return "Fizz"
+  elsif (int.to_i % 3 != 0) && (int.to_i % 5 == 0)
+    return "Buzz"
+  end
+end
