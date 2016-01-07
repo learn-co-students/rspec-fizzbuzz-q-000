91b2ee3724870edf63191c0cad6817f312d8df32
jakeekaj <jakemalto2014@gmail.com>
2016-01-07 11:30:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
-def fizzbuzz
+def fizzbuzz(x)
+  if x % 3 == 0 && x % 5 == 0
+     "FizzBuzz"
+  elsif x % 5 == 0
+     "Buzz"
+  elsif x % 3 == 0
+     "Fizz"
+  else
+    return nil
+  end
+
 end
