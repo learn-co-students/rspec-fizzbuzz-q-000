3803854f59478d48f73eb7094933fa0ab1ee2b55
cguirgui <cyril.guirguis@gmail.com>
2016-01-16 20:33:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,15 @@
 def fizzbuzz(int)
 
   fizz_15 = "FizzBuzz"
-fizz_3 = "Fizz"
+  fizz_3 = "Fizz"
   fizz_5 = "Buzz"
+
   if (int % 5 == 0) && (int % 3 == 0)
   return fizz_15
   end
-
-    if int % 3 == 0
-    return fizz_3
+  
+  if int % 3 == 0
+  return fizz_3
   end
   
   if int % 5 == 0
