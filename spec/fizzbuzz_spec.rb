267a835f1f3033f93d23d36b555829ddc926bacc
emill3000 <emill3000@yahoo.com>
2016-03-03 09:41:55 -0800

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,4 +1,6 @@
+
 require_relative './spec_helper.rb'
+
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
@@ -22,3 +24,4 @@ describe "fizzbuzz" do
     expect(fizz_4).to eq(nil)
   end
 end
+
