88482396e9e911a53777e69ccf7403dd2654e1c9
Shmo Chuang <elisachuang@spotify.com>
2016-03-12 11:16:44 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,15 @@
 def fizzbuzz(num)
-    string = nil
+    string = ""
     if num % 3 == 0
         string = string + "Fizz"
     end
     
     if num % 5 == 0
         string = string + "Buzz"
+    end
+    
+    if num % 3 != 0 && num % 5 != 0
+        string = nil
     end
     
     return string
