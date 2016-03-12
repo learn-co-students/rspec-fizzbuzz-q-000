1fef8519bc7539bf688f67d327267a66692aa5fe
Dom-Mc <dom.mckellar@gmail.com>
2016-03-11 19:54:53 -0800

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(num)
+  if (num % 3 == 0) && (num % 5 == 0)
+    fizz_15 = "FizzBuzz"
+  elsif num % 3 == 0
+    fizz_3 = "Fizz"
+  elsif num % 5 == 0
+    fizz_5 = "Buzz"
+  else
+    fizz_4 = nil
+  end
+
+end
