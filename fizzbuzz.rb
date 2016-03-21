ad53bc4441592c46d07fd5e29c4b115ba81c3113
jmaldia <jmaldia@activecampaign.com>
2016-03-20 22:16:47 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(number)
+  # while number > 0
+    if number % 5 == 0 && number % 3 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
+    elsif number % 5 == 0
+      "Buzz"
+    else
+    end
+
+    # number -= 1
+  # end
+end
+
+fizzbuzz(35)
+    
+
+      
