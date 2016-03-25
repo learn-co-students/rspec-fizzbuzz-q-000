fe59db652e0605a0b2a67ae77fa8bc8ec5618c42
conuska <onuskacc@gmail.com>
2016-03-25 15:40:11 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  else if int % 3 == 0
+    return "Fizz"
+  else if int % 5 == 0
+    return"Buzz"
+  end
+end
+end
+end
+
