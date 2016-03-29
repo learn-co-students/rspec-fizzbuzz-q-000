22cbffe8654dd7b2b03c4816667f4818d750dcf2
Marshall Bergeron <bergeronvt@gmail.com>
2016-03-28 20:07:18 -0400

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(x)
+    if x % 3 == 0 and x % 5 == 0
+        return "FizzBuzz"
+    elsif x % 5 == 0
+        return "Buzz"
+    elsif x % 3 == 0
+        return "Fizz"
+    else
+        return nil
+    end
+end
+        
+    
