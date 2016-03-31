1d89143f452ab3d512c1275aed2c94bba299c703
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-03-31 17:47:16 -0400

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+    
+
+    def fizzbuzz(i)
+      if i % 5 == 0 and i % 3 == 0
+        "FizzBuzz"
+      elsif i % 5 == 0
+        "Buzz"
+      elsif i % 3 == 0
+        "Fizz"
+      else
+      end
+    end
