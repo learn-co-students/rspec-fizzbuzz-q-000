66b74bbc6c7c4500fe41ca0e1805eaf3ad58438a
Josue Magallanes <josue_m_23@hotmail.com>
2016-05-28 05:18:09 -0400

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+def fizzbuzz(testnum)
+  if testnum % 5 == 0 && testnum % 3 == 0
+    "FizzBuzz"
+elsif testnum % 5 == 0
+    "Buzz"
+elsif   testnum % 3 == 0
+  "Fizz"
+  else
+    puts ""
+  end
+end
