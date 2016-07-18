68e80f1ae108c541e893b4256ff78c9aa32237a4
Jennell Lewis <jennellrenai@gmail.com>
2016-07-18 04:46:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
-def fizzbuzz()
-  if int % 3 == 0
-    puts "Fizz"
-  if int % 5 == 0
-    puts "Buzz"
-  if int % 3 == 0 && 5 == 0
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
   else
-    puts " "
+    puts"nil"
+  end
 end
