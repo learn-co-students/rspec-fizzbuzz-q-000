93bf00be807743a478ce0bf7f88efa1267d93860
Mike Robey <robey.mc@gmail.com>
2016-07-24 22:02:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,16 @@
-number = gets.strip
-
 def fizzbuzz(number)
 
 if number.to_i % 15 == 0
-puts "FizzBuzz"
+return "FizzBuzz"
 
 elsif number.to_i % 3 == 0
-puts "Fizz"
+return "Fizz"
 
 elsif number.to_i % 5 == 0
-puts "Buzz"
+return "Buzz"
 
 else
 puts "nil"
 
 end
 end
-
-fizzbuzz (number)
