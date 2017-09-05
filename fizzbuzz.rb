00c474e7b1d8563cf546c68801adbd09055ef308
smithwebtek <brad@smithwebtek.com>
2017-09-05 15:34:53 -0400

    refactor, remove %15

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,10 @@
 def fizzbuzz(value)
   if (value % 3 == 0) && (value % 5 != 0)
   "Fizz" 
-  
   elsif (value % 5 == 0) && (value % 3 != 0)
   "Buzz" 
-  
-  elsif value % 15 == 0
-  "FizzBuzz"
-
   elsif value % 3 == value % 5
   "FizzBuzz" 
-  
   else 
   end
 end
