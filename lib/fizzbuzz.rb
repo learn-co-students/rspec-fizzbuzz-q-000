3d03ec464e1ceff09208a142a7e3d9b4833e2077
Amy Carney <amy.carney@alaska.gov>
2017-12-14 01:32:31 +0000

    Done.

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,14 @@
+# require 'pry'
+
+def fizzbuzz(int)
+  # binding.pry
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
+end
